f5195055fdd2b90e09d1bd735afc22fa6a42ef1f
shokiogawa <syoki2578@outlook.jp>
2020-08-24 10:34:24 +0900

    aaa

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,5 @@
 class Topic < ApplicationRecord
-  belongs_to :textbook, dependent: :destroy
+  belongs_to :textbook
   has_many :t_contents, dependent: :destroy
   has_many :exams, dependent: :destroy
   validates :title, presence: true, length: {maximum: 255}
